0e6869285b8cbac7f5f94e11a58094164a861706
oukkurun <oukkurun@gmail.com>
2023-09-25 19:59:54 -0300

    texto implementado para que diga las stats

--- Assets/Scripts/textCanva.cs
+++ Assets/Scripts/textCanva.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class textCanva : MonoBehaviour
 {
+    public Text text;
+    public Chara mainchara;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text.text = "Vida: " + mainchara.devolverVida() + "Ataque: " + mainchara.devolverAtaque() + "Defensa: " + mainchara.devolverDefensa();
     }
 
     // Update is called once per frame
